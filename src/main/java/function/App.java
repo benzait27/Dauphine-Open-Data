7db8151fe9f6096167ba7f07c3f81267666d72fb
Zakaria BENZAIT <benzait.zakaria.93@gmail.com>
2017-11-06 17:42:28 +0100

    Start new project and init both of maven and git

---
+++ src/main/java/function/App.java
@@ -0,0 +1,17 @@
+package function;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+	@SuppressWarnings("unused")
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		new App().proceed();
+	}
+
+	public void proceed() {
+		System.out.println("Hello World!");
+	}
+}
