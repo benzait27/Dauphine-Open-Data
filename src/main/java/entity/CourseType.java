4f3ce20be7d42311f92ab216f63e7c8c5c4ab606
Zakaria BENZAIT <benzait.zakaria.93@gmail.com>
2017-11-09 17:41:44 +0100

    #1 this class indicate the type the course as TD, TP, and the volume of each type (hours)

---
+++ src/main/java/entity/CourseType.java
@@ -0,0 +1,28 @@
+package entity;
+
+public class CourseType {
+	
+	private TypeCourse type;
+	private int volume;
+	
+	public CourseType(TypeCourse type, int volume) {
+		super();
+		this.type = type;
+		this.volume = volume;
+	}
+	public TypeCourse getType() {
+		return type;
+	}
+	public void setType(TypeCourse type) {
+		this.type = type;
+	}
+	public int getVolume() {
+		return volume;
+	}
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+	
+	
+
+}
