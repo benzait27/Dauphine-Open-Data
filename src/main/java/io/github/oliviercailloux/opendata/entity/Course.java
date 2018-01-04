aa6573dcf1a9dc5c625ba606ef1b5efe82e8cd88
Zakaria BENZAIT <benzait.zakaria.93@gmail.com>
2018-01-04 22:35:22 +0100

    update files

---
+++ src/main/java/io/github/oliviercailloux/opendata/entity/Course.java
@@ -0,0 +1,127 @@
+package entity;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+@XmlRootElement(name="Course", namespace="Course2")
+@Entity
+
+public class Course  implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String idCourse;
+	// the description course
+	private String description;	
+	// replace String author by Person author (Basics)
+	private Person author;
+	// the name of the course
+	private String courseName;
+	// All the course type as CM, TD and TP and their volume (number of hours for each type)
+	private Set<CourseType> courseTypes = new HashSet<CourseType>() ;
+	// list of the course's references
+	private Set<String> references = new HashSet<String>();
+	// the language of the course in witch it' written 
+	@Enumerated(EnumType.STRING)
+	private TeachingLang teachingLangs ;
+	// the course's credit
+	private int credits;
+	 
+	public Course() {
+		
+		}
+
+	public Course(String idCourse, String description, Person author, String courseName, HashSet<CourseType> courseTypes,
+			HashSet<String> references, TeachingLang teachingLangs, int credits) {
+		this.idCourse = idCourse;
+		this.description = description;
+		this.author = author;
+		this.courseName = courseName;
+		this.courseTypes = courseTypes;
+		this.references = references;
+		this.teachingLangs = teachingLangs;
+		this.credits = credits;
+	}
+
+	// put the idCourse on property of the element
+	@XmlAttribute(name="idCourse")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	public String getIdCourse() {
+		return idCourse;
+	}
+
+	public void setIdCourse(String idCourse) {
+		this.idCourse = idCourse;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Person getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Person author) {
+		this.author = author;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public Set<CourseType> getCourseTypes() {
+		return courseTypes;
+	}
+
+	public void setCourseTypes(Set<CourseType> courseTypes) {
+		this.courseTypes = courseTypes;
+	}
+
+	public Set<String> getReferences() {
+		return references;
+	}
+
+	public void setReferences(Set<String> references) {
+		this.references = references;
+	}
+
+	public TeachingLang getTeachingLangs() {
+		return teachingLangs;
+	}
+
+	public void setTeachingLangs(TeachingLang teachingLangs) {
+		this.teachingLangs = teachingLangs;
+	}
+
+	public int getCredits() {
+		return credits;
+	}
+
+	public void setCredits(int credits) {
+		this.credits = credits;
+	}
+
+	
+	
+}
---
+++ src/main/java/io/github/oliviercailloux/opendata/servlet/AppServlet.java
@@ -0,0 +1,36 @@
+package servlet;
+
+/**
+ * Created by Ziad on 07/11/2017.
+ */
+
+import entity.Teaching;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.MediaType;
+
+@WebServlet("/appServlet")
+public class AppServlet  extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        resp.setContentType(MediaType.TEXT_PLAIN);
+        final ServletOutputStream out = resp.getOutputStream();
+
+        // TRY TEACHING FORMAT ...
+        Teaching teaching = new Teaching(null, LocalDateTime.now(),"","" );
+
+        out.println("TEST : "+teaching.getDateWithFormat("dd MM YYYY"));
+        out.println("TEST null : "+teaching.getDateWithFormat(null));
+    }
+}
+
+
