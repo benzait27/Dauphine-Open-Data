39e701a4d6404a511893732e32b6c92e7d2c9a65
BENZAIT Zakaria <bz_benzait@esi.dz>
2018-01-06 18:38:57 +0100

    Update testQuerey.java

--- src/main/java/io/github/oliviercailloux/opendata/servlet/testQuerey.java
+++ src/main/java/io/github/oliviercailloux/opendata/servlet/testQuerey.java
@@ -26,7 +26,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dao.ObjectDao;
+import io.github.oliviercailloux.opendata.dao.ObjectDao;
 
 
 
