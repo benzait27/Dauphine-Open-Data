8ef2a4e0230801cc30b6eb8354f23ca60ea9c87d
Zakaria BENZAIT <benzait.zakaria.93@gmail.com>
2018-02-10 20:25:59 +0100

    Implement methods allow us to read and write Triple from the BDD, #11

---
+++ src/main/java/io/github/oliviercailloux/opendata/servlet/testTriple.java
@@ -0,0 +1,95 @@
+package io.github.oliviercailloux.opendata.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import io.github.oliviercailloux.opendata.entity.FunctionTypes;
+import io.github.oliviercailloux.opendata.entity.Person;
+import io.github.oliviercailloux.opendata.entity.Triple;
+import io.github.oliviercailloux.opendata.utils.JPAutil;
+import io.github.oliviercailloux.opendata.utils.ServletHelper;
+
+/**
+ * Servlet implementation class testTriple
+ */
+@WebServlet("/testTriple")
+public class testTriple extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public testTriple() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		@SuppressWarnings("resource")
+		final ServletOutputStream out = new ServletHelper().configureAndGetOutputStream(response);
+		out.println("Start DAO opretation");
+		out.flush();
+		
+		JPAutil j= new JPAutil();
+		EntityManager entityManager=j.getEntityManager("Dauphine-Open-Data");
+
+		// add all the Persons int the BDD
+		EntityTransaction tx = entityManager.getTransaction();
+		Triple t=   new Triple();
+		t.setObject("This is the first triple");
+	
+		Triple t1=   new Triple();
+		t1.setObject("This is the first triple");
+		
+	 	tx.begin();
+	 	entityManager.persist(t);
+	 	entityManager.persist(t1);
+	 	tx.commit();
+		
+	 	//get a Person from the BDD
+	 	Triple t4 = entityManager.find(t.getClass(),"1") ;
+        out.println(t4.getObject());
+       
+        
+    	// delete a Triple:
+        tx.begin();
+        Triple TripleDeleted =entityManager.merge(t); 
+        entityManager.remove(TripleDeleted);
+        tx.commit();
+      
+        
+        //Update Triple: change the Ojbect of the Triple
+    	t1.setObject("This the modified triple");
+    	tx.begin();
+	 	entityManager.merge(t1);
+	 	tx.commit();
+	 	 
+	    //get All the Triples
+	     List<Triple> Objects =entityManager.createQuery("select c from Triple c").getResultList();
+	 	 out.println(Objects.get(0).getObject());
+	 	 
+	 	
+	 	 out.println("End All Operation Triple.");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
